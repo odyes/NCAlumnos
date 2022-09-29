092c1d080481b32ca807c86e6946ec494a86b482
Helliud <javiereliudliz@gmail.com>
2022-09-29 08:02:01 -0700

    nice

--- NavControllerAlumno/DetallesAlumnoController.swift
+++ NavControllerAlumno/DetallesAlumnoController.swift
@@ -8,9 +8,8 @@
 
 import UIKit
 
-class DetallesAlumnosController : UIViewController{
+class DetallesAlumnosController : UIViewController, UITableViewDelegate, UITableViewDataSource{
     
-    @IBOutlet var tvAlumnos: UIView!
     @IBOutlet weak var lblEdad: UILabel!
     @IBOutlet weak var lblCarrera: UILabel!
     @IBOutlet weak var lblMatricula: UILabel!
@@ -29,5 +28,24 @@ class DetallesAlumnosController : UIViewController{
             self.title = "Detalles de Alumnos"
         }
         
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 88
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return alumno!.materias.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tableView.dequeueReusableCell(withIdentifier: "celdaMateria") as? CeldaMateriasController
+        celda?.lblNombre.text = alumno!.materias[indexPath.row].nombre
+        celda?.lblCodigo.text = alumno!.materias[indexPath.row].codigo
+        return celda!
     }
 }
